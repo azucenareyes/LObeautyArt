f5ae6c6642ccfc618670a2f0557610976c33af92
Azucena Martinez <azucena_rys@yahoo.com>
2018-03-17 12:10:29 -0600

    added buttons that don't work

---
+++ stylist/src/bio_paragraphs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Bio = (props) => {
+
+    return(
+      <p>
+        Born with a natural ability to create and make things beautiful.
+        Lydia 1st generation /self made beauty artist, found her calling early
+        in life being in charge of younger family members and helping them
+        groom themselves. Doing hair styles and putting makeup on neighborhood
+        girls and some boys,  while they performed little talent shows in
+        her front yard. In high school she would do her friends hair and makeup
+        for special occasions and photography.
+
+        Born in Colorado working in the Longmont and Denver areas most of
+        her 16 year career. Lydia has educated, mentored stylists and
+        students,managed businesses, worked global and national runway shows
+        and salon owned.
+
+        Lydia’s education and background in the beauty industry upon her
+        degree in Cosmetology in ‘03, is working with Tigi/Bedhead for 5 years
+        as a international cosmetic artist and educator. Worked for Enjoy
+        International for 2 years as a brand ambassador and colorist,Owned and
+        operated Ella’s Hair Studio for 8years. She is experienced in hair care
+        and cosmetic lines Schwarzopf, Tigi, Framesi, Joico, L’Oreal, Lorac,
+        NYX and many others.
+      
+        Lydia's current freelance business is LObeautyArt..she's practicing
+        her Master Stylist skills at On The Avenue Salon in longmont, CO.
+        performing the latest trend in hair color and cutting, makeup
+        artistry, Up-dos for special occasions., specializing in Vintage hair
+        styles, waxing and hair extensions.
+      </p>
+    );
+};
+
+export default Bio;
